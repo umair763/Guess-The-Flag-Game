90cdcf819b0cd1e7751807561b971126d553200a
Muhammad Umair <93200938+umair763@users.noreply.github.com>
2024-05-11 14:57:51 +0500

    Responsive CSS-Update

--- src/components/InitialFlagContainer.jsx
+++ src/components/InitialFlagContainer.jsx
@@ -1,25 +1,30 @@
 // InitialFlagContainer.jsx
-import React from 'react';
-import './InitialFlagContainer.css';
+import React from "react";
+import "./InitialFlagContainer.css";
 
 function InitialFlagContainer({ onStartClick }) {
-    return (
-        <>
-            <div className="card">
-                <div className="img-container">
-                    <img src="\src\components\pk.png" alt="Pakistan Flag" />
-                    <div className="outer-container">
-                        <p className="Theh1">Guess The Flag</p>
-                        <button className="startbtn" onClick={onStartClick}>
-                            Start
-                        </button>{' '}
-                        {/* Call onStartClick when button clicked */}
-                        <p className="p-text">20 Questions</p>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+	return (
+		<>
+			<div className="card ">
+				<div className="bgimg">
+					<img
+						src="\src\components\World countries flags stock illustration.jpeg"
+						alt="background-img"
+					/>
+				</div>
+				<div className="outer-container">
+					<p className="Theh1">Guess The Flag</p>
+					<button
+						className="startbtn"
+						onClick={onStartClick}>
+						Start
+					</button>{" "}
+					{/* Call onStartClick when button clicked */}
+					<p className="p-text">20 Questions</p>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default InitialFlagContainer;
